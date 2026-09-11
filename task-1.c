ff7de7af3198f56c6548190b50db38477bbba246
Yasir Abbasi <pc9304.muhammadyasir@gmail.com>
2026-09-11 19:35:04 +0500

    first commit

---
+++ task-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int units = 650;
+    if (units > 500) {
+        printf("High Usage");
+    }
+    else {
+        printf("Normal Usage");
+    }
+
+    return 0;
+}
